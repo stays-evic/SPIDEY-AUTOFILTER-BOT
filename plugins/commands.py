d870ad821bb93dfb6184ae97bca58e9b031d2188
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-01-10 09:26:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -316,7 +316,7 @@ async def start(client:Client, message):
                 howtodownload = settings.get('tutorial_2', TUTORIAL_2) if is_second_shortener else settings.get('tutorial', TUTORIAL)
             buttons = [[
                 InlineKeyboardButton(text="✅ Free Token ✅", url=verify),
-                InlineKeyboardButton(text="ʜᴏᴡ ᴛᴏ Get Token❓", url=howtodownload)
+                InlineKeyboardButton(text="ʜᴏᴡ ᴛᴏ ɢᴇᴛ ꜰʀᴇᴇ ᴛᴏᴋᴇɴ❓", url=howtodownload)
                 ],[
                 InlineKeyboardButton(text="😁 ʙᴜʏ sᴜʙsᴄʀɪᴘᴛɪᴏɴ - ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴠᴇʀɪғʏ 😁", callback_data='seeplans'),
             ]]
@@ -905,7 +905,7 @@ async def all_settings(client, message):
 📍1 ᴛᴜᴛᴏʀɪᴀʟ ʟɪɴᴋ - {settings['tutorial']}
 
 📍2 ᴛᴜᴛᴏʀɪᴀʟ ʟɪɴᴋ - {settings['tutorial_2']}
-mm
+
 📍3 ᴛᴜᴛᴏʀɪᴀʟ ʟɪɴᴋ - {settings['tutorial_3']}
 
 🎯 ɪᴍᴅʙ ᴛᴇᴍᴘʟᴀᴛᴇ - `{settings['template']}`
