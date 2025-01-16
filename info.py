1580fd0fa78941e5b37e5481d64f76688a78c79f
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-01-16 23:22:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,9 +43,9 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/AllnewForward_bot') #Su
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/Howtodownlaodmp4/1443")
-TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/Howtodownlaodmp4/1443")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/Howtodownlaodmp4/1443")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/Howtodownlaodmp4/1446")
+TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/Howtodownlaodmp4/1446")
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/Howtodownlaodmp4/1446")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 SHORTENER_API = environ.get("SHORTENER_API", "f7bedc6aa86e3f7bd8bbe8fa0906e03c75213cb9")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'mdiskshortner.link')
