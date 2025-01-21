21a1eb22fa45eff6dade68c318530b8860c802e9
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-01-21 17:09:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -191,6 +191,9 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Tᴏᴘ Tʀᴇɴᴅɪɴɢ 📊', callback_data="trending")
                         ]] 
                         reply_markup = InlineKeyboardMarkup(buttons)
+                        m=await message.reply_sticker("CAACAgUAAxkBAAEMvy1m1TaC8G2H7gXLgxUF4VrYyb69awAC2BMAAiT2qVYv7An51W_jpTUE") 
+                        await asyncio.sleep(1)
+                        await m.delete)(
                         await message.reply_photo(photo=random.choice(START_IMG), caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
                             reply_markup=reply_markup,
                             parse_mode=enums.ParseMode.HTML)
@@ -270,6 +273,9 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Tᴏᴘ Tʀᴇɴᴅɪɴɢ 📊', callback_data="trending")
                         ]] 
         reply_markup = InlineKeyboardMarkup(buttons)
+        m=await message.reply_sticker("CAACAgUAAxkBAAEMvy1m1TaC8G2H7gXLgxUF4VrYyb69awAC2BMAAiT2qVYv7An51W_jpTUE") 
+                        await asyncio.sleep(1)
+                        await m.delete()
         return await message.reply_photo(photo=START_IMG, caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
