1e5305803e608ba7f6d323d1b43f2032b494d2b1
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-02-12 12:07:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://maximum-minnie-nikkulazy-dd714bca.koyeb.app/")
+URL = environ.get("FQDN", "https://worthy-brunhilda-nikkulazy-a9b061e8.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
