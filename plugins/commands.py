f51f5ee1766b41442c600de397becaa6cacbef28
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-02-18 08:29:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -342,7 +342,7 @@ async def start(client:Client, message):
                 file_caption=file.caption
             )
             btn = [[
-                InlineKeyboardButton("꧁༺WATCH & DOWNLOAD ༻꧂", callback_data=f'stream#{file.file_id}')
+                InlineKeyboardButton("🖥️WATCH & DOWNLOAD📥", callback_data=f'stream#{file.file_id}')
             ]]
             toDel = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -382,7 +382,7 @@ async def start(client:Client, message):
         file_caption=files.caption
     )
     btn = [[
-        InlineKeyboardButton("꧁༺WATCH & DOWNLOAD ༻꧂", callback_data=f'stream#{file_id}')
+        InlineKeyboardButton("🖥️WATCH & DOWNLOAD📥", callback_data=f'stream#{file_id}')
     ]]
     toDel=await client.send_cached_media(
         chat_id=message.from_user.id,
